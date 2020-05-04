18a4a5f3d8f87cb42a775614050b5f8d8a540466
theo97two <theo.moundras.1@ulaval.ca>
2020-05-03 23:18:03 -0400

    ajout

--- Site web/scrapper.py
+++ Site web/scrapper.py
@@ -1,178 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import random
 
+# ('MAT-1910',  'Math de ingenieur I', 3, 4, 'MAT-0260', 'GLO,GIF,GEL,GPH,GEX,GCI,GMC,GMN,GIN', 'o,o,o,o,o,o,o,o,o')
 
+lienprog = [
+    'https://www.ulaval.ca/les-etudes/cours/repertoire/detailsCours/gel-3003-signaux-et-systemes-discrets.html#renseignements'#,
+    #'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-logiciel-b-ing.html#description-officielle&structure-programme',
+    # 'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-des-eaux/',
+    # 'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-cooperatif-en-genie-des-mines-et-de-la-mineralurgie/',
+    # 'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-geologique/'
+    # ,'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-logiciel/',
+    # 'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-cooperatif-en-genie-des-materiaux-et-de-la-metallurgie/'
+]
+def tablecours(variable):
 
+    newurl = variable
+    newresponse = requests.get(newurl)
+    soups = BeautifulSoup(newresponse.text, 'html5lib')
+    a = soups.find('div', {'id': 'contenuFiche'})
+    code = a.find('h1', {'class': 'fiche'}).contents
+    sigle = code[0][0:8]
+    nomcours = a.find('h1', {'class': 'fiche'}).find('span').text
 
-url = 'https://www.fsg.ulaval.ca/etudes/genie/'
+    credit = a.find('div', {'class': 'bloc renseignements'}).find('div', {'class' : 'droite'}).contents
+    vraicredit = credit[0]
 
-response = requests.get(url)
-if response.ok:
-    ref, lienprogramme = [], []
-    soup = BeautifulSoup(response.text, 'lxml')
-    b = soup.findAll('a', {'class': 'btn-pour-en-savoir-plus'})
-    for a in b:
-        lien = a['href']
-        ref.append('https://www.fsg.ulaval.ca' + lien)
-    for a in ref:
-        newurl = a
-        newresponse = requests.get(newurl)
-        soups = BeautifulSoup(newresponse.text, 'lxml')
-        c = soups.findAll('a', {'class': 'bouton bouton-deuxtierstiers fond-rouge'})
-        for d in c:
-            liens = d['href']
-            if liens.find('baccalaureat') != -1 and not 'https://www.fsg.ulaval.ca' + liens in lienprogramme:
-                lienprogramme.append('https://www.fsg.ulaval.ca' +liens)
-                #('MAT-1910',  'Math de ingenieur I', 3, 4, 'MAT-0260', 'GLO,GIF,GEL,GPH,GEX,GCI,GMC,GMN,GIN', 'o,o,o,o,o,o,o,o,o')
+    return sigle, nomcours, int(vraicredit), random.randrange(0, 10)
 
-    lienprog = ['https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-chimique/'#,
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-civil/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-des-eaux/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-cooperatif-en-genie-des-mines-et-de-la-mineralurgie/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-geologique/'
-                #,'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-logiciel/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-cooperatif-en-genie-des-materiaux-et-de-la-metallurgie/'
-                ]
-    for un in lienprog:
-        obligatoire, option, = [], []
-        newurl = un
-        newresponse = requests.get(newurl)
-        soups = BeautifulSoup(newresponse.text, 'lxml')
-        c0 = soups.findAll('section', {'class': 'strate fond-gris-bleu-pale'})
-        for i in c0:
-            war = i.find('header').find('h1').text
-            print(war)
-            break
-        for i in c0:
-
-            var = i.findAll('section', {'class': 'strate-contenu'} )
-            for z in var:
-                var1 = z.findAll('article', {'class': 'accordeon fond-gris-bleu-pale'})
-                for w in var1:
-                    var2 = w.find('button', {'type': 'button'}, )
-                    if var2.text == 'Structure du programme':
-
-                        var3 = w.findAll('header')
-                        for r in var3:
-
-                            if r.find('h4').text == 'Activités de formation communes':
-
-                                var4 = r.parent.find('table', {'class': 'property'}).findAll('a')
-                                for lien in var4:
-                                    liens = lien['href']
-                                    if not liens in obligatoire:
-                                        obligatoire.append(liens)
-                                print(obligatoire)
-                            if r.find('h4').text == 'Autres activités':
-                                var4 = r.parent.find('table', {'class': 'block'}).findAll('h6', {'class': 'regle'})
-                                a = []
-                                for lien in var4:
-
-                                    bc = []
-
-                                    if not (lien.text == 'Règle 3 - 3 crédits' or lien.text == 'Règle 2 - 3 crédits' or lien.text == 'Règle 1 - 3 crédits'
-                                    or lien.text == 'Règle 4 - 3 crédits' or lien.text == 'Règle 5 - 3 crédits' or lien.text == 'Règle 6 - 3 crédits' ) or not lien.parent.find('span', {
-                                        'class': 'ulavalExigenceCoursCode'}):
-
-
-                                        var5 = lien.parent.find('table', {'class': 'property'}).findAll('a')
-
-                                        for otherlien in var5:
-                                            liens = otherlien['href']
-                                            if not liens in bc:
-                                                bc.append(liens)
-
-                                        if lien.text[10] != '0' and not lien.text[11].isdigit:
-
-
-                                            newvar = lien.text[10]
-
-                                            a.append({lien.text[0:7]: {newvar + ' crédits': bc}})
-                                        if lien.text[10] == '0':
-                                            newvar = lien.text[14]
-                                            a.append({lien.text[0:7]: {newvar + ' crédits': bc}})
-                                        elif lien.text[10] != '0' and lien.text[11].isdigit:
-                                            newvar = lien.text[10] + lien.text[11]
-                                            a.append({lien.text[0:7]: {newvar + ' crédits': bc}})
-
-                                print(a)
-
-                            if r.find('h4').text == 'Concentrations':
-
-                                concentrations = {}
-                                listeconcentration = []
-                                var4 = r.parent.findAll('table', {'class': 'block'})
-                                for zb in var4:
-                                    az = zb.find('h5') #concentration
-
-                                    aa = az.parent.parent.parent.parent.findAll('h6', {'class': 'regle'}) #listerègle
-                                    concentration = {}
-                                    for lien in aa:
-
-
-                                        bcd = []
-                                        if lien.text == 'Règle 1 - 3 crédits'and lien.parent.find('div', {'class': 'en-evidence regroupement'}): # sans concentration
-
-                                            var5 = lien.parent.parent.find('table', {'class': 'property'}).findAll('a')
-                                            for otherlien in var5:
-                                                liens = otherlien['href']
-
-                                                if not liens in bcd:
-                                                    bcd.append(liens)
-                                            newvar = lien.parent.parent.parent.parent.parent.find('span', {'class': 'h5'}).text
-
-                                            concentration['None'] = {newvar + ' crédits': bcd}
-                                            concentrations[az.text] = concentration
-
-                                        if not lien.parent.find('div', {'class': 'en-evidence regroupement'}):
-
-
-                                            var5 = lien.parent.find('table', {'class': 'property'}).findAll('a')
-
-                                            for otherlien in var5:
-                                                liens = otherlien['href']
-                                                if not liens in bcd:
-                                                     bcd.append(liens)
-
-                                            if lien.text[10] == '0':
-
-                                                newvar = lien.text[14]
-                                                concentration[lien.text[0:7]] = {newvar + ' crédits': bcd}
-                                                concentrations[az.text] = concentration
-
-                                            elif lien.text[10] != '0' and not lien.text[11].isdigit:
-
-                                                newvar = lien.text[10]
-
-                                                concentration[lien.text[0:7]] = {newvar + ' crédits': bcd}
-                                                concentrations[az.text] = concentration
-                                            elif lien.text[10] != '0' and lien.text[11].isdigit:
-                                                newvar = lien.text[10] + lien.text[11]
-                                                concentration[lien.text[0:7]] = {newvar + ' crédits': bcd}
-                                                concentrations[az.text] = concentration
-
-
-
-
-
-
-                                print(concentrations)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(tablecours('https://www.ulaval.ca/les-etudes/cours/repertoire/detailsCours/gel-3003-signaux-et-systemes-discrets.html#renseignements'))
--- Site web/scrapper2.py
+++ Site web/scrapper2.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-from selenium import webdriver
+import scrapper
 
 
 
@@ -10,13 +9,15 @@ from selenium import webdriver
 
                 #('MAT-1910',  'Math de ingenieur I', 3, 4, 'MAT-0260', 'GLO,GIF,GEL,GPH,GEX,GCI,GMC,GMN,GIN', 'o,o,o,o,o,o,o,o,o')
 
-lienprog = ['https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-chimique-b-ing.html?tx_oful_pi1%5Bprint%5D=1'#,
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-civil/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-des-eaux/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-cooperatif-en-genie-des-mines-et-de-la-mineralurgie/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-geologique/'
-                #,'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-en-genie-logiciel/',
-                #'https://www.fsg.ulaval.ca/etudes/programmes-detudes/baccalaureat-cooperatif-en-genie-des-materiaux-et-de-la-metallurgie/'
+lienprog = ['https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-chimique-b-ing.html?tx_oful_pi1%5Bprint%5D=1',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-logiciel-b-ing.html#description-officielle&structure-programme',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-industriel-b-ing.html#description-officielle&structure-programme',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-civil-b-ing.html#description-officielle&structure-programme',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-des-eaux-b-ing.html#description-officielle&structure-programme'
+                ,'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-electrique-b-ing.html#description-officielle&structure-programme',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-geologique-b-ing.html#description-officielle&structure-programme',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-informatique-b-ing.html#description-officielle&structure-programme',
+                'https://www.ulaval.ca/les-etudes/programmes/repertoire/details/baccalaureat-en-genie-physique-b-ing.html#description-officielle&structure-programme'
                 ]
 for un in lienprog:
     obligatoire, option, concentration = [], [], []
@@ -32,8 +33,7 @@ for un in lienprog:
     print("Le programme choisi est " + nomprogramme)
     for variante in b:
         if variante.find('div', {'class' : 'titresection'}).text == 'Activités de formation communes':
-            c = variante.parent.find(lambda tag: tag.name == 'table' and
-                                   tag.get('class') == ['cours']).findAll('a')
+            c = variante.findAll('a')
             for var in c:
                 db = var['href']
                 if not 'https://www.ulaval.ca' + db in obligatoire:
@@ -43,17 +43,32 @@ for un in lienprog:
         if variante.find('div', {'class': 'titresection'}).text == 'Autres activités':
             c = variante.findAll('div', {'class' : 'regle'})
             for var in c:
+                dbnew = 0
                 liste = []
-                nomregle = var.find('div', {'class' : 'titreregle'}).text
-                if nomregle == 'Règle 1 - 3 crédits' or nomregle == 'Règle 2 - 3 crédits' or nomregle == 'Règle 3 - 3 crédits' \
-                    or nomregle == 'Règle 4 - 3 crédits'or nomregle == 'Règle 5 - 3 crédits'or nomregle == 'Règle 6 - 3 crédits'or \
-                    nomregle == 'Règle 7 - 3 crédits' or nomregle == 'Règle 3 - Un cours parmi:':
-                    dbnew = 3
+                numeroregle = []
+                nombrecredit = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+                for op in range(11):
+                    numeroregle.append(op)
 
-                else:
-                    dbnew = int(nomregle[nomregle.find(' - ')+2:nomregle.find(' à')])
-                    if dbnew == 0:
-                        dbnew = int(nomregle[nomregle.find('à ')+2:nomregle.find(' crédits')])
+                nomregle = var.find('div', {'class': 'titreregle'}).text
+
+                for abc in numeroregle:
+                    for aze in nombrecredit:
+                        for azb in nombrecredit:
+
+                            if nomregle == 'Règle ' + str(abc) + ' - ' + str(aze) + ' crédits':
+                                dbnew = aze
+                            if nomregle == 'Règle ' + str(abc) + ' - ' + str(aze) + ' crédits parmi:':
+                                dbnew = aze
+                            if nomregle == 'Règle 3 - Un cours parmi:':
+                                dbnew = 3
+                            if nomregle == 'Règle ' + str(abc) + ' - ' + str(aze) + ' à ' + str(
+                                    azb) + ' crédits parmi:':
+
+                                if aze == 0:
+                                    dbnew = azb
+                                else:
+                                    dbnew = aze
 
 
 
@@ -75,25 +90,53 @@ for un in lienprog:
             c = variante.findAll(lambda tag: tag.name == 'div' and
                                                  tag.get('class') == ['bloc'])
             for vari in c:
+                compteur = 0
+                comparaison = []
+
                 listeregle = []
                 nomconcentration = vari.find('td', {'class' : 'col1'}).text
                 creditconcentration = vari.find('td', {'class' : 'col3'}).text
-                regle = vari.findAll('div', {'class' : 'regle'})
-                for z in regle:
+                oblig = vari.find('table', {'class': 'cours'}).findAll('a')
+                for uni in oblig:
+                    db = uni['href']
 
+                    if not 'https://www.ulaval.ca' + db in comparaison:
+                        comparaison.append('https://www.ulaval.ca' + db)
+
+
+                regle = vari.findAll('div', {'class' : 'regle'})
+
+
+                for z in regle:
+                    compteur += 1
+                    dbnew = 0
                     liste = []
+                    numeroregle = []
+                    nombrecredit = [0, 3, 6, 7, 9, 12, 15, 18, 21, 24, 27, 30]
+                    for op in range(11):
+                        numeroregle.append(op)
 
                     nomregle = z.find('div', {'class': 'titreregle'}).text
 
-                    if nomregle == 'Règle 1 - 3 crédits' or nomregle == 'Règle 2 - 3 crédits' or nomregle == 'Règle 3 - 3 crédits' \
-                            or nomregle == 'Règle 4 - 3 crédits' or nomregle == 'Règle 5 - 3 crédits' or nomregle == 'Règle 6 - 3 crédits' or \
-                            nomregle == 'Règle 7 - 3 crédits' or nomregle == 'Règle 3 - Un cours parmi:':
-                        dbnew = 3
 
-                    else:
-                        dbnew = int(nomregle[nomregle.find(' - ') + 2:nomregle.find(' à')])
-                        if dbnew == 0:
-                            dbnew = int(nomregle[nomregle.find('à ') + 2:nomregle.find(' crédits')])
+                    for abc in numeroregle:
+                        for aze in nombrecredit:
+                            for azb in nombrecredit:
+
+                                if nomregle == 'Règle ' + str(abc) + ' - ' + str(aze) + ' crédits':
+
+                                    dbnew = aze
+                                if nomregle == 'Règle ' + str(abc) + ' - ' + str(aze) + ' crédits parmi:':
+                                    dbnew = aze
+                                if nomregle == 'Règle 3 - Un cours parmi:':
+                                    dbnew = 3
+                                if nomregle == 'Règle ' + str(abc) + ' - ' + str(aze) + ' à ' + str(azb) + ' crédits parmi:':
+
+                                    if aze == 0:
+                                        dbnew = azb
+                                    else:
+                                        dbnew = aze
+
 
                     o = z.findAll('a')
 
@@ -104,12 +147,18 @@ for un in lienprog:
 
                         if not 'https://www.ulaval.ca' + db in liste:
                             liste.append('https://www.ulaval.ca' + db)
+                    if comparaison != liste and compteur == 1:
+                        creditobli = 0
+                        for i in comparaison:
+                            creditobli += 3
+                        if not {'obligatoire': {str(creditobli) + ' crédits': comparaison}} in listeregle:
+                            listeregle.append({'obligatoire': {str(creditobli) + ' crédits': comparaison}})
                     listeregle.append({nomregle[0:7]: {str(dbnew) + ' crédits': liste}})
-                concentration.append({nomconcentration: {creditconcentration: listeregle}})
+                concentration.append({nomconcentration: {creditconcentration + ' crédits': listeregle}})
             print(concentration)
 
 
-    
+
 
 
 
