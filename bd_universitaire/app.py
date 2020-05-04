08a08b3ec44bb23a77f12858d50b71c87ab874c0
BeastBeno <awoussidietzbenony@gmail.com>
2020-05-04 02:35:13 -0400

    flask

--- bd_universitaire/app.py
+++ bd_universitaire/app.py
@@ -1,4 +1,4 @@
-
+'''
 from flask import Markup
 from flask import Flask
 from flask import render_template
@@ -22,3 +22,14 @@ if __name__ == "_main_":
 
 if __name__ == "__main__":
     app.run()
+
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+app.run(debug=True)
