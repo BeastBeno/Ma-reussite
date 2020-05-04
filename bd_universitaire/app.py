41aef96b1be7cd5adb9846ce450fec629091fb23
BeastBeno <awoussidietzbenony@gmail.com>
2020-05-04 02:04:15 -0400

    Changement du flask

--- bd_universitaire/app.py
+++ bd_universitaire/app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Reussite2019'
+app.config['MYSQL_PASSWORD'] = 'pr@j?vision100%@$!'
 app.config['MYSQL_DB'] = 'bd_universitaire'
 
 mysql = MySQL(app)
 @app.route('/bd_universitaire/', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html', msg='')
+    return render_template('../mon site web/Sanstitre-2.html', msg='')
 
-
-
+if __name__ == "__main__":
+    app.run()
